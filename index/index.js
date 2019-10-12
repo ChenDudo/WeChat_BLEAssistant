17b68f8ebf3447c98c40f6a026d8d457d410da76
cd103 <1421027452@qq.com>
2019-10-12 18:00:52 +0800

    新增服务搜寻

--- index/index.js
+++ index/index.js
@@ -122,9 +122,12 @@ Page({
       deviceId,
       success: (res) => {
         for (let i = 0; i < res.services.length; i++) {
+          console.log('ble services OK', res)
           if (res.services[i].isPrimary) {
             this.getBLEDeviceCharacteristics(deviceId, res.services[i].uuid)
-            return
+          }
+          if(res.services[i].isSubPrimary){
+            this.getBLEDeviceCharacteristics(deviceId, res.services[i].uuid)
           }
         }
       }
@@ -155,6 +158,9 @@ Page({
             this.writeBLECharacteristicValue()
           }
           if (item.properties.notify || item.properties.indicate) {
+            this._deviceId = deviceId
+            this._serviceId = serviceId
+            this._characteristicId = item.uuid
             wx.notifyBLECharacteristicValueChange({
               deviceId,
               serviceId,
@@ -194,7 +200,8 @@ Page({
     // 向蓝牙设备发送一个0x00的16进制数据
     let buffer = new ArrayBuffer(1)
     let dataView = new DataView(buffer)
-    dataView.setUint8(0, Math.random() * 255 | 0)
+    // dataView.setUint8(0, Math.random() * 255 | 0)
+    dataView.setUint8(1,2)
     wx.writeBLECharacteristicValue({
       deviceId: this._deviceId,
       serviceId: this._serviceId,
@@ -206,4 +213,44 @@ Page({
     wx.closeBluetoothAdapter()
     this._discoveryStarted = false
   },
+
+  getNotifydata(){
+  //   // 获取 BLE notify
+  //   // wx.notifyBLECharacteristicValueChange({
+  //   //   state: true, // 启用 notify 功能
+  //   //   // 这里的 deviceId 需要已经通过 createBLEConnection 与对应设备建立链接
+  //   //   deviceId: this._deviceId,
+  //   //   // 这里的 serviceId 需要在 getBLEDeviceServices 接口中获取
+  //   //   serviceId: "00000000-fc0a-4c04-9df8-245fc68a5036",
+  //   //   // 这里的 characteristicId 需要在 getBLEDeviceCharacteristics 接口中获取
+  //   //   characteristicId: "00000001-fc0a-4c04-9df8-245fc68a5036",
+  //   //   success(res) {
+  //   //     console.log('notifyBLECharacteristicValueChange success', res.errMsg)
+  //   //   }
+  //   // })
+  },
+
+  // openLED
+  openLED() {
+    let buffer = new ArrayBuffer(2)
+    let dataView = new DataView(buffer)
+    dataView.setUint8(1,2)
+    wx.writeBLECharacteristicValue({
+      deviceId: "ED:67:17:27:AE:31",
+      serviceId: "00000000-fc0a-4c04-9df8-245fc68a5036",
+      characteristicId: "00000002-fc0a-4c04-9df8-245fc68a5036",
+      value: buffer,
+    })
+  },
+
+
+
+
+
+
+
+
+
+
+
 })
